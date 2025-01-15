eb50a256cd0f2c42e350891fc63e3bc0b13cb82a
CWilloughby98 <carloswl1998@gmail.com>
2025-01-15 15:59:09 +0100

    fixed logout route export

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -1,10 +1,12 @@
 import { supabase } from "@/app/lib/supabase"
 import { redirect } from "next/navigation";
 
-export const logout = async () => {
+const logout = async () => {
     const { error } = await supabase.auth.signOut();
 
     if (error) console.log(error)
 
     redirect("/")
 }
+
+export default logout;
